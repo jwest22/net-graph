d0761719078a7e5a32aae628e2cf494d131eff84
Jake <jweststewart@gmail.com>
2023-11-27 07:21:49 -0700

    added streamlit support

---
+++ main.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import steiner_tree_demo
+
+st.title('Steiner Tree Demo')
+    
+def main():
+    
+    similarity_index_control = st.slider('Similarity Index Relation Threashold',min_value=0.00,max_value=1.01, value=1.01)
+    
+    # Check if the state variables exist, otherwise initialize them
+    if 'start-nodes-key' not in st.session_state:
+        st.session_state['start-nodes-key'] =  'users.email'
+    if 'target-nodes-key' not in st.session_state:
+        st.session_state['target-nodes-key'] = 'order_details.product_id'
+
+    # Create two text input fields
+    start_nodes = st.text_input("Starting Node(s)", 'users.email')
+    target_nodes = st.text_input("Target Node(s)", 'order_details.product_id')
+    if start_nodes is not None:
+        st.session_state['start-nodes-key'] = start_nodes
+    if target_nodes is not None:
+        st.session_state['target-nodes-key'] = target_nodes
+        
+    if st.button("Draw Graph"):
+        steiner_tree_demo.auto_relation(similarity_index_control,st.session_state['start-nodes-key'],st.session_state['target-nodes-key'])
+        HtmlFile = open("front.html", 'r', encoding='utf-8')
+        source_code = HtmlFile.read() 
+        components.html(source_code, height = 1500,width=1500)
+            
+main()
--- steiner_tree_demo.py
+++ steiner_tree_demo.py
@@ -3,13 +3,14 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from itertools import combinations
 from networkx.algorithms.approximation import steiner_tree
+from pyvis.network import Network
 
-# Set similarity index threshold for edge creation. Set to greater than 1 to disable. 
-similarity_index = 0.5
+ # Set similarity index threshold for edge creation. Set to greater than 1 to disable. 
+#similarity_index = 0.5
 
 # Set terminal nodes to find path
-start_nodes = {'users.email'}
-target_nodes = {'order_details.product_id'}
+#start_nodes = {'users.email'}
+#target_nodes = {'order_details.product_id'} 
 
 # Load CSV files into pandas DataFrames
 users_df = pd.read_csv('data/users.csv')
@@ -27,16 +28,13 @@ dfs = {
     'addresses': addresses_df
 }
 
-# Create a graph
-G = nx.Graph()
-
 # Function to calculate the Jaccard Index
 def calculate_jaccard_index(set1, set2):
     intersection = len(set1.intersection(set2))
     union = len(set1.union(set2))
     return intersection / union if union != 0 else 0
 
-def add_graph_items(table_name, df):
+def add_graph_items(G,table_name, df):
     # Add nodes to the graph for each column
     columns = df.columns
     for col in columns:
@@ -56,65 +54,96 @@ def calculate_jaccard(G, node1, node2):
     return intersection / union if union != 0 else 0
 
 # Add inter-table relationships based on column name and connect them
-def add_inter_table_relationships(graph, dataframes):
+def add_inter_table_relationships(G, dataframes):
     for table1, df1 in dataframes.items():
         for table2, df2 in dataframes.items():
             if table1 != table2:
                 common_columns = set(df1.columns).intersection(df2.columns)
                 for col in common_columns:
-                    graph.add_edge(f"{table1}.{col}", f"{table2}.{col}",weight=1,color='blue')
+                    G.add_edge(f"{table1}.{col}", f"{table2}.{col}",weight=1,color='blue')
 
-# Add base items to graph
-for table_name, df in dfs.items():
-    add_graph_items(table_name, df)
+def auto_relation(similarity_index,start_nodes,target_nodes):
+    # Create a graph
+    G = nx.Graph()
+
+    # Add base items to graph
+    for table_name, df in dfs.items():
+        add_graph_items(G,table_name, df)
+        
+    # Add inter-table relationships to graph                  
+    add_inter_table_relationships(G, dfs)
+
+    # Add Jaccard relations to graph - Iterate over all combinations of nodes to calculate Jaccard index
+    for node1, node2 in combinations(G.nodes(), 2):
+        jaccard_index = calculate_jaccard(G, node1, node2)
+        if jaccard_index > similarity_index and not G.has_edge(node1, node2):
+            # Add an edge for Jaccard index above similarity_index variable
+            G.add_edge(node1, node2, weight=jaccard_index,color='yellow')
+        
+    """ 
+    # Print the edges with Jaccard index above similarity_index variable
+    print("Jaccard Index Edges Added:")
+    for u, v, weight in G.edges(data='weight'):
+        if weight is not None and weight > similarity_index:
+            print(f"({u}, {v}) -> {weight:.8f}")
+
+    print("Graph created successfully!")
+    print("Number of nodes:", G.number_of_nodes())
+    print("Number of edges:", G.number_of_edges()) 
+    """
     
-# Add inter-table relationships to graph                  
-add_inter_table_relationships(G, dfs)
+    start_nodes_split = start_nodes.split(',')
+    target_nodes_split = target_nodes.split(',')
 
-# Add Jaccard relations to graph - Iterate over all combinations of nodes to calculate Jaccard index
-for node1, node2 in combinations(G.nodes(), 2):
-    jaccard_index = calculate_jaccard(G, node1, node2)
-    if jaccard_index > similarity_index and not G.has_edge(node1, node2):
-        # Add an edge for Jaccard index above similarity_index variable
-        G.add_edge(node1, node2, weight=jaccard_index,color='yellow')
+    terminal_nodes = start_nodes_split + target_nodes_split
+    # Compute the Steiner Tree
+    st_tree = steiner_tree(G, terminal_nodes, method='mehlhorn')
+
+    main_edges = G.edges()
+    main_edge_colors = [G[u][v]['color'] for u,v in main_edges]
     
-# Print the edges with Jaccard index above similarity_index variable
-print("Jaccard Index Edges Added:")
-for u, v, weight in G.edges(data='weight'):
-    if weight is not None and weight > similarity_index:
-        print(f"({u}, {v}) -> {weight:.8f}")
+    for u, v in st_tree.edges():
+        if G.has_edge(u, v):
+            G[u][v]['color'] = 'red'
 
-print("Graph created successfully!")
-print("Number of nodes:", G.number_of_nodes())
-print("Number of edges:", G.number_of_edges())
-print("Graph Edges:")
-print(G.edges())
+    # Draw the main graph
+    plt.figure(figsize=(15, 15))
+    pos = nx.spring_layout(G, k=0.15, iterations=25)  # positions for all nodes
+    nx.draw_networkx_nodes(G, pos)
+    nx.draw_networkx_edges(G, pos, edgelist=G.edges(), edge_color=main_edge_colors)
+    nx.draw_networkx_labels(G, pos)
 
-terminal_nodes = start_nodes | target_nodes
-# Compute the Steiner Tree
-st_tree = steiner_tree(G, terminal_nodes, method='mehlhorn')
+    # Highlight the edges of the Steiner Tree
+    nx.draw_networkx_edges(G, pos, edgelist=st_tree.edges())
 
-edges = G.edges()
-colors = [G[u][v]['color'] for u,v in edges]
+    """
+    title_string_start = ', '.join(start_nodes_split)
+    title_string_target =  ', '.join(target_nodes_split)
+    title_string = "Network Steiner Tree: " + title_string_start + " to " + title_string_target + '''
+    Black = Intra-Table, Blue  = Inter-table, Yellow = Jaccard Relation, Red = Steiner Tree
+    '''
 
-# Draw the original graph
-plt.figure(figsize=(10, 10))
-pos = nx.spring_layout(G, k=0.15, iterations=25)  # positions for all nodes
-nx.draw_networkx_nodes(G, pos)
-nx.draw_networkx_edges(G, pos, edgelist=G.edges(), edge_color=colors)
-nx.draw_networkx_labels(G, pos)
+    plt.title(title_string)
+    plt.axis("off")
+    """
+    
+    for node, data in G.nodes(data=True):
+        for key, value in data.items():
+            if isinstance(value, set):
+                # Convert set to list
+                G.nodes[node][key] = list(value)
 
-# Highlight the edges of the Steiner Tree
-nx.draw_networkx_edges(
-    G, pos, edgelist=st_tree.edges(), width=3, alpha=0.5, edge_color="r"
-) 
+    for u, v, data in G.edges(data=True):
+        for key, value in data.items():
+            if isinstance(value, set):
+                # Convert set to list
+                G[u][v][key] = list(value)
+        
+    net = Network(heading='')
+    net.from_nx(G)  # Assuming S is your Steiner tree or the graph you want to visualize
 
-title_string_start = ', '.join(start_nodes)
-title_string_target =  ', '.join(target_nodes)
-title_string = "Network Steiner Tree: " + title_string_start + " to " + title_string_target + '''
-  Black = Intra-Table, Blue  = Inter-table, Yellow = Jaccard Relation, Red = Steiner Tree
-'''
+    # Customize the network (optional, you can add more customization as needed)
+    #net.show_buttons(filter_=['physics'])  # This line will show buttons to manipulate physics settings interactively
 
-plt.title(title_string)
-plt.axis("off")
-plt.show()
+    # Save and show the network
+    net.save_graph("front.html")
